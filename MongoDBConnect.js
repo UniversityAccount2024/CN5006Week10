0086a255ffe11f329f69703ba1a8825d5cd6b1dc
UniversityAccount2024 <u2484105@uel.ac.uk>
2024-12-03 14:35:43 +0000

    Add files via upload

---
+++ MongoDBConnect.js
@@ -0,0 +1,17 @@
+mongoose = require('mongoose')
+const MONG_URI = "mongodb://localhost:27017/lab_Iek6"
+
+mongoose.connect(MONG_URI, {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true,
+    useFindAndMondify: false
+})
+
+const db = mongoose.connection;
+db.on("error", function(err){
+    console.log("Error occured" + err)
+})
+
+db.once("connected", function(){
+    console.log("Connection is successful to " + MONG_URI)
+})
